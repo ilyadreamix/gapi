fd9213e8344a0b1e2f14f63267624eca8a8ff8ec
ilyadreamix <ilyabelij2504@gmail.com>
2024-08-18 13:11:52 +0300

    update gradle

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,3 +9,13 @@ version = libs.versions.gapi.get()
 repositories {
     mavenCentral()
 }
+
+dependencies {
+    api(libs.kotlinLogging)
+    api(libs.kotlinx.serialization.json)
+    api(libs.ktor.client.core)
+    api(libs.ktor.client.cio)
+    api(libs.ktor.client.contentNegotiation)
+    api(libs.ktor.client.json)
+    api(libs.ktor.client.logging)
+}
